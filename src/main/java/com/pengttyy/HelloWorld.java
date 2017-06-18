0dbfffef4c4d102433ecc1ba1694f0062c5a5315
pengttyy <pengttyy@gmail.com>
2017-06-18 15:35:17 +0800

    feat(helloWord):第一个springbootHelloworld

---
+++ src/main/java/com/pengttyy/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.pengttyy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by pengt on 2017/6/18.
+ */
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/hello")
+    public String say() {
+        return "spring-boot hello";
+    }
+
+}
