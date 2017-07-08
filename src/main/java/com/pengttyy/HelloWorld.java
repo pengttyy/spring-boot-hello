2b6e2d5b181ecbe2789b8832a0e4ab695110faec
pengttyy <pengttyy@gmail.com>
2017-07-08 22:24:57 +0800

    feat:整合swagger2生成接口文档

--- src/main/java/com/pengttyy/HelloWorld.java
+++ src/main/java/com/pengttyy/HelloWorld.java
@@ -1,17 +1,21 @@
 package com.pengttyy;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 /**
  * Created by pengt on 2017/6/18.
  */
 @RestController
 public class HelloWorld {
-
+    private static final Random random = new Random();
     @GetMapping("/hello")
+    @ApiOperation(value = "say hello", notes = "这是一个测试接口")
     public String say() {
-        return "spring-boot hello";
+        return "spring-boot hello" + random.nextInt();
     }
 
 }
--- src/main/java/com/pengttyy/SpringBootHelloApplication.java
+++ src/main/java/com/pengttyy/SpringBootHelloApplication.java
@@ -2,9 +2,39 @@ package com.pengttyy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringBootHelloApplication {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.pengttyy"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        Contact contact = new Contact("pengttyy", "http://localhost", "pengttyy@gmail.com");
+        return new ApiInfoBuilder()
+                .title("Spring Boot集成Swagger2")
+                .description("Hello Swagger2")
+                .contact(contact)
+                .version("1.0")
+                .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootHelloApplication.class, args);
